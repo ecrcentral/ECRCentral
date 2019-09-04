cff55b14a5eac912687f1f85c83693f4b3a71ad8
asntech <azez.khan@gmail.com>
2019-09-04 14:30:18 +0100

    added career level model

---
+++ app/Models/CareerLevel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+
+class CareerLevel extends Model
+{
+	protected $guarded = [
+        'id',
+    ];
+
+
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    /**
+     * The purpose that belong to the travel grant.
+     */
+    public function travelgrants()
+    {
+        return $this->belongsToMany(TravelGrant::class, 'careerlevel_travelgrant');
+    }
+
+    public function parentId()
+    {
+        return $this->belongsTo(self::class);
+    }
+
+    
+}
